00d7812b349a9b9b9f8655d260e272b7ace0101c
ivankumar123 <kumarivan56@gmail.com>
2026-02-24 10:55:26 +0530

    Deleting unwanted files

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,22 +1,11 @@
-import { Routes, Route } from 'react-router'
-import { Homepage } from '/pages/Homepage.jsx'
-import { Checkout } from '/pages/Checkout.jsx'
-import { Orderspage } from '/pages/Orderspage.jsx'
-import { Tracking } from '/pages/Tracking.jsx'
+
 import './App.css'
 
 function App() {
   
 
   return (
-    <Routes>
-      <Route index element={<Homepage />} />
-      <Route path="Checkout" element={<Checkout />} />
-      <Route path="orders" element={<Orderspage />} />
-      <Route path="tracking" element={<Tracking />} />
-    </Routes>
-  
-    
+    <h1>hello world</h1>
   )
 }
 
